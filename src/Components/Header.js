3672cb5bbd24d492eae5d84195b24ce743b95807
mitulpatel224 <mitul.patel224@gmail.com>
2022-12-30 20:05:50 +0530

    feat: project setup

---
+++ src/Components/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h2>IP Address Tracker</h2>
+      <div className="form-group">
+        <input
+          type="text"
+          name="ipAddress"
+          placeholder="Search for any IP address or domain"
+        />
+        <button className="btn btn-primary" type="submit">
+          <span className="icon-arrow-right"></span>
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
